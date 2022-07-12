25bdcf3e8733ad9b9dce78a57927278525161608
PabloCoppe <pablo.steiner@coppe.ufrj.br>
2022-07-12 15:29:35 -0300

    Create raiox_iptu.py

---
+++ raiox_iptu.py
@@ -0,0 +1,5 @@
+import streamlit as st 
+
+st.title('Entendo o IPTU da Cidade do Rio de Janeiro')
+st.markdown('Você sabe quantos imóveis estão cadastrados no IPTU?')
+st.write('De fato, o Rio é uma cidade plural ')
